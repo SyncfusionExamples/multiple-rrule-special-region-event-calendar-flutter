f4661154b887e5289a343232d5d86f78060a95fb
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-07-14 15:17:55 +0530

    sample added

---
+++ android/app/src/main/kotlin/com/example/flutter_recurrence_special/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_recurrence_special
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ScheduleExample();
+}
+
+class ScheduleExample extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SfCalendar(
+          view: CalendarView.week,
+          dataSource: getCalendarDataSource(),
+          specialRegions: _getTimeRegions(),
+        ),
+      ),
+    );
+  }
+
+  _DataSource getCalendarDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now(),
+      endTime: DateTime.now().add(const Duration(hours: 1)),
+      subject: 'Meeting',
+      color: Colors.pink,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 4)),
+      endTime: DateTime.now().add(const Duration(hours: 5)),
+      subject: 'Release Meeting',
+      color: Colors.lightBlueAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6)),
+      endTime: DateTime.now().add(const Duration(hours: 7)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime(2020, 1, 22, 1, 0, 0),
+      endTime: DateTime(2020, 1, 22, 3, 0, 0),
+      subject: 'Support',
+      color: Colors.green,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime(2020, 1, 24, 3, 0, 0),
+      endTime: DateTime(2020, 1, 24, 4, 0, 0),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+
+  List<TimeRegion> _getTimeRegions() {
+    final List<TimeRegion> regions = <TimeRegion>[];
+    regions.add(TimeRegion(
+        startTime: DateTime(2020, 5, 29, 00, 0, 0),
+        endTime: DateTime(2020, 5, 29, 24, 0, 0),
+        recurrenceRule: 'FREQ=WEEKLY;INTERVAL=1;BYDAY=SAT,SUN',
+        color: Color(0xffbD3D3D3),
+        enablePointerInteraction: true,
+        ));
+    regions.add(TimeRegion(
+        startTime: DateTime(2020, 5, 29, 18, 0, 0),
+        endTime: DateTime(2020, 5, 29, 24, 0, 0),
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+        color: Color(0xffbD3D3D3),
+        enablePointerInteraction: true,
+        ));
+    regions.add(TimeRegion(
+        startTime: DateTime(2020, 5, 29, 00, 0, 0),
+        endTime: DateTime(2020, 5, 29, 09, 0, 0),
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+       color: Color(0xffbD3D3D3),
+        enablePointerInteraction: true,
+        ));
+    regions.add(TimeRegion(
+        startTime: DateTime(2020, 5, 29, 13, 0, 0),
+        endTime: DateTime(2020, 5, 29, 14, 0, 0),
+        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+        color: Color(0xffbD3D3D3),
+        enablePointerInteraction: true,
+        ));
+
+    return regions;
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
